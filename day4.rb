c03dcd7a0449cd0e5da45761267a572001a2124d
Konrad Winkler <konwinkler@gmail.com>
2022-12-04 20:41:40 -0500

    day 4 part 2

--- day4.rb
+++ day4.rb
@@ -25,7 +25,7 @@ def to_range(input)
 end
 test_equals(to_range('1-2'), 1..2)
 
-def overlapping_pairs(fileName)
+def overlapping_pairs(fileName, full_overlap = true)
     lines = read_file(fileName)
     ranges = lines.map {|x|
         first, second = x.split(",")
@@ -33,10 +33,26 @@ def overlapping_pairs(fileName)
     }
     overlaps = ranges.map {|x|
         left, right = x
-        full_overlap?(left, right)
+        if full_overlap
+            full_overlap?(left, right)
+        else
+            some_overlap?(left, right)
+        end
     }
     overlaps.select {|x| x}.length
 end
 
 test_equals(overlapping_pairs('input4-example.txt'), 2)
 puts "part 1 #{overlapping_pairs('input4.txt')}"
+
+def some_overlap?(left, right)
+    intersection = left.to_a & right.to_a
+    !intersection.empty?
+end
+test_equals(some_overlap?(1..1, 2..2), false)
+test_equals(some_overlap?(1..2, 2..2), true)
+test_equals(some_overlap?(1..2, 2..3), true)
+test_equals(some_overlap?(2..2, 2..3), true)
+
+test_equals(overlapping_pairs('input4-example.txt', false), 4)
+puts "part 2 #{overlapping_pairs('input4.txt', false)}"
