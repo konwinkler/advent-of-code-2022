809eafa078836e99b5247fba51be241f639d1b15
Konrad Winkler <konwinkler@gmail.com>
2022-12-08 21:11:33 -0500

    day 8 part 2

--- day8.rb
+++ day8.rb
@@ -57,3 +57,44 @@ end
 
 test_equals(visible_trees('input8-example.txt'), 21)
 puts "part 1 #{visible_trees('input8.txt')}"
+
+def scenic_score(x, y, map)
+    width = map[0].length - 1
+    height = map.length - 1
+    if x == 0 || y == 0 || x == width || y == height
+        return 0
+    end
+    tree = map[y][x]
+    see_up = ((y-1).downto(0).find_index {|i| map[i][x] >= tree} || (y-1)) + 1
+    see_down = ((y+1..height).find_index {|i| map[i][x] >= tree} || (height-y-1)) + 1
+    see_right = ((x+1..width).find_index {|i| map[y][i] >= tree} || (width-x-1)) + 1
+    see_left =  ((x-1).downto(0).find_index {|i| map[y][i] >= tree} || (x-1)) + 1
+    # binding.pry if x == 2 && y == 3
+    return see_up * see_down * see_right * see_left
+end
+test_equals(scenic_score(0, 1, [[2,2,2],[2,2,2],[2,2,2]]), 0)
+test_equals(scenic_score(1, 1, [[2,2,2],[2,2,2],[2,2,2],[2,2,2]]), 1)
+test_equals(scenic_score(1, 1, [[2,2,2],[2,2,2],[2,1,2],[2,2,2]]), 2)
+test_equals(scenic_score(1, 2, [[2,2,2],[2,1,2],[2,2,2],[2,2,2]]), 2)
+test_equals(scenic_score(1, 1, [[2,2,2,2],[2,2,1,2],[2,2,2,2],[2,2,2,2]]), 2)
+test_equals(scenic_score(2, 1, [[2,2,2,2],[2,1,2,2],[2,2,2,2],[2,2,2,2]]), 2)
+test_equals(scenic_score(1, 1, [[2,2,2,2],[2,2,1,2],[2,1,2,2],[2,2,2,2]]), 4)
+test_equals(scenic_score(2, 1, [[2,2,2,2],[1,1,2,2],[2,2,1,2],[2,2,2,2]]), 4)
+test_equals(scenic_score(2, 2, [[2,2,1,2],[2,2,1,2],[2,1,2,2],[2,2,2,2]]), 4)
+
+def highest_scenic_score(file_name)
+    lines = read_file(file_name)
+    map = lines.map {|line| line.split('').map {|x| x.to_i}}
+    copy = lines.map {|line| line.split('').map {|x| x.to_i}}
+    map.each_with_index {|row, y|
+        row.each_with_index{|tree, x|
+            score = scenic_score(x, y, map)
+            copy[y][x] = score
+        }
+    }
+    # binding.pry
+    copy.flatten.max
+end
+
+test_equals(highest_scenic_score('input8-example.txt'), 8)
+puts "part 2 #{highest_scenic_score('input8.txt')}"
