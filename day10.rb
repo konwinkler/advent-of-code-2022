ce1fa17f1c5f31d2e7a5f5390e7e8c66d727e106
Konrad Winkler <konwinkler@gmail.com>
2022-12-10 20:22:14 -0500

    day 10 part 2

--- day10.rb
+++ day10.rb
@@ -61,3 +61,62 @@ end
 
 test_equals(sum_signals('input10-example.txt'), 13140)
 puts "part 1 #{sum_signals('input10.txt')}"
+
+def render(file_name)
+    instructions = []
+    lines = read_file(file_name)
+    lines.map {|line|
+        operator, amount = line.split ' '
+        if operator == 'noop'
+            instruction = Instruction.new(1, 0)
+        else
+            instruction = Instruction.new(2, amount.to_i)
+        end
+        instructions.push instruction
+    }
+
+    cycle = 0
+    x = 1
+    instruction = nil
+    screen = []
+    current_screen = ''
+    while !instructions.empty?
+        cycle += 1
+        if instruction == nil
+            instruction = instructions.shift
+        end
+        # during the cycle before x got adjusted
+        if (cycle - 1) % 40 == 0
+            screen.push current_screen
+            current_screen = ''
+            # binding.pry
+        end
+        draw_at = (cycle - 1) % 40
+        if draw_at >= (x - 1) && draw_at <= (x + 1)
+            current_screen += '#'
+        else
+            current_screen += '.'
+        end
+        instruction.current_cycle += 1 
+        if instruction.current_cycle == instruction.cycles_needed
+            x += instruction.amount
+            instruction = nil
+        end
+    end
+    screen.shift # drop the first empty line
+    screen.push current_screen # add the in progress line
+    screen
+end
+
+
+example_solution = [
+    '##..##..##..##..##..##..##..##..##..##..',
+    '###...###...###...###...###...###...###.',
+    '####....####....####....####....####....',
+    '#####.....#####.....#####.....#####.....',
+    '######......######......######......####',
+    '#######.......#######.......#######.....',
+]
+test_equals(render('input10-example.txt'), example_solution)
+puts 'part 2'
+puts render('input10.txt')
