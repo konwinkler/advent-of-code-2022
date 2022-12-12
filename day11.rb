d902bbbfd997d1e6863daa9eb92bc09f319c9c41
Konrad Winkler <konwinkler@gmail.com>
2022-12-11 21:39:34 -0500

    day 11 part 1

--- day11.rb
+++ day11.rb
@@ -10,13 +10,25 @@ def test_equals(actual, expected)
     raise "test failed, actual: #{actual} expected: #{expected}" unless actual == expected
 end
 
+$item_counter = 0
+
 class Item
     attr_accessor :id
     attr_accessor :worry_level
 
-    def initialize(id)
-        @id = id
-        @worry_level = 0
+    def initialize(worry_level)
+        @id = $item_counter
+        @worry_level = worry_level
+        $item_counter += 1
+    end
+
+    def adjust_worry(worry_operation)
+        @worry_level = worry_operation.(@worry_level)
+        @worry_level = @worry_level / 3
+    end
+
+    def test_worry(decision_test)
+        decision_test.(@worry_level)
     end
 end
 
@@ -36,52 +48,59 @@ class Monkey
         @decision_test = decision_test
         @throw_directions = throw_directions
     end
+
+    def inspect_item
+        @inspection_count += 1
+    end
 end
 
 def parse(lines)
     monkeys = []
-    items = []
-    divisible_by = 0
-    worry_operation = nil
-    decision_test = nil
-    throw_direction = {}
-    lines.each_with_index do |line, index|
-        current_monkey = index / 7
-        monkey_part = index % 7
-        case monkey_part
-        when 1
-            parts = line.split ' '
-            parts = parts.drop(2)
-            items = parts.map {|value| Item.new(value.to_i)}
-        when 2
-            parts = line.split ' '
-            amount = parts.pop.to_i
-            op = parts.pop
-            if op == '+'
-                worry_operation = lambda {|old_worry| old_worry + amount}
-            elsif op == '*'
-                worry_operation = lambda {|old_worry| old_worry * amount}
-            else
-                raise "operation parse error, #{op}"
-            end
-        when 3
-            divisible_by = line.split(' ').pop.to_i
-            decision_test = lambda {|value| (value.to_f / divisible_by) % 1 == 0}
-        when 4
-            throw_to = line.split(' ').pop.to_i
-            throw_direction[true] = throw_to
-        when 5
-            throw_to = line.split(' ').pop.to_i
-            throw_direction[false] = throw_to
-        when 6
-            monkey = Monkey.new(current_monkey, items, worry_operation, decision_test, throw_direction)
-            monkeys.push monkey
-            items = []
-            divisible_by = 0
-            worry_operation = nil
-            decision_test = nil
-            throw_direction = {}
-        end         
+    slices = lines.each_slice(7)
+    slices.each_with_index do |slice, current_monkey|
+        items = []
+        divisible_by = 0
+        worry_operation = nil
+        decision_test = nil
+        throw_direction = {}
+        slice.each_with_index do |line, monkey_part|
+            case monkey_part
+            when 1
+                parts = line.split ' '
+                parts = parts.drop(2)
+                items = parts.map {|value| Item.new(value.to_i)}
+            when 2
+                parts = line.split ' '
+                amount = parts.pop
+                op = parts.pop
+                if op == '+'
+                    if (amount == 'old')
+                        worry_operation = lambda {|old_worry| old_worry + old_worry}
+                    else
+                        worry_operation = lambda {|old_worry| old_worry + amount.to_i}
+                    end
+                elsif op == '*'
+                    if (amount == 'old')
+                        worry_operation = lambda {|old_worry| old_worry * old_worry}
+                    else
+                        worry_operation = lambda {|old_worry| old_worry * amount.to_i}
+                    end
+                else
+                    raise "operation parse error, #{op}"
+                end
+            when 3
+                divisible_by = line.split(' ').pop.to_i
+                decision_test = lambda {|value| (value.to_f / divisible_by) % 1 == 0}
+            when 4
+                throw_to = line.split(' ').pop.to_i
+                throw_direction[true] = throw_to
+            when 5
+                throw_to = line.split(' ').pop.to_i
+                throw_direction[false] = throw_to
+            end         
+        end    
+        monkey = Monkey.new(current_monkey, items, worry_operation, decision_test, throw_direction)
+        monkeys.push monkey
     end
     monkeys
 end
@@ -89,6 +108,28 @@ end
 def level_moneky_business(file_name)
     lines = read_file(file_name)
     monkeys = parse(lines)
-    binding.pry
+
+    # 20 rounds
+    (1..20).each do |round|
+        monkeys.each_with_index do |monkey|
+            monkey.items.each do |item|
+                item.adjust_worry(monkey.worry_operation)
+                monkey.inspect_item
+                target = monkey.throw_directions[item.test_worry(monkey.decision_test)]
+                monkeys[target].items.push item
+                # puts "monkey #{monkey.id} throws #{item.worry_level} to #{target}"
+            end
+            monkey.items = []
+        end
+        if false
+            puts "After round #{round}"
+            monkeys.each do |monkey|
+                puts "Monkey #{monkey.id}: items #{monkey.items.map {|i| i.worry_level}}"
+            end
+        end
+    end
+    inspection_counts = monkeys.map {|monkey| monkey.inspection_count}
+    inspection_counts.sort.last(2).reduce(:*)
 end
 test_equals(level_moneky_business('input11-example.txt'), 10605)
+puts "part 1 #{level_moneky_business('input11.txt')}"
