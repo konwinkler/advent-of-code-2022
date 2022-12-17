82fbe5e0370adf090be3646480eab9a41b777be9
Konrad Winkler <konwinkler@gmail.com>
2022-12-17 16:55:31 -0500

    day 17 part 1

--- day17.rb
+++ day17.rb
@@ -16,9 +16,12 @@ end
 
 Chamber = Struct.new(:tiles) do
     def height
-        tiles.reduce(0) {|h, c| [h, c.y]}
+        tiles.reduce(0) {|highest, tile| [highest, tile[1]].max}
     end
 end
+test_equals(Chamber.new([]).height, 0)
+test_equals(Chamber.new([Vector[1, 1]]).height, 1)
+
 $gust_map = {
     '<' => Vector[-1, 0],    
     '>' => Vector[1, 0]
@@ -42,22 +45,26 @@ class Rock
     def fall(chamber)
         fall_direction = Vector[0, -1]
         new_tiles = @tiles.map {|x| x + fall_direction}
-        blocked = false
+        did_fall = true
         # hit bottom?
-        blocked = true if new_tiles.select {|x| x[y] == 0}.any?
+        did_fall = false if new_tiles.select {|x| x[1] == 0}.any?
         # hit other tile?
-        blocked = true if new_tiles.select {|x| chamber.tiles.include? x}.any?
-        blocked
+        did_fall = false if new_tiles.select {|x| chamber.tiles.include? x}.any?
+        if did_fall
+            @tiles = new_tiles
+        end
+        did_fall
+    end
 end
-test_rock = Rock.new([Vector[0, 1]])
+test_rock = Rock.new([Vector[0, 2]])
 test_chamber = Chamber.new([])
-test_equals(test_rock.push('>', test_chamber).tiles, [Vector[1, 1]])
-test_equals(test_rock.push('<', test_chamber).tiles, [Vector[0, 1]])
-test_equals(test_rock.push('<', test_chamber).tiles, [Vector[0, 1]])
+test_equals(test_rock.push('>', test_chamber).tiles, [Vector[1, 2]])
+test_equals(test_rock.push('<', test_chamber).tiles, [Vector[0, 2]])
+test_equals(test_rock.push('<', test_chamber).tiles, [Vector[0, 2]])
 
 test_equals(test_rock.fall(test_chamber), true)
 test_equals(test_rock.fall(test_chamber), false)
-test_equals(test_rock.tiles, [Vector[0, 0]]])
+test_equals(test_rock.tiles, [Vector[0, 1]])
 
 class Jet
     @counter
@@ -99,22 +106,65 @@ test_equals(test_ceiling.next.tiles, [Vector[1, 1]])
 test_equals(test_ceiling.next.tiles, [Vector[2, 2], Vector[3, 3]])
 test_equals(test_ceiling.next(5, 5).tiles, [Vector[6, 6]])
 
+def draw(chamber)
+    h = chamber.height
+    lines = []
+    h.downto(1).each do |y|
+        line = ''
+        (0..6).each do |x|
+            line += (chamber.tiles.include? Vector[x, y]) ? '#' : ' '
+        end
+        lines.push line
+    end
+    puts
+    puts lines
+    puts
+end
+
 def height_after(file_name, target_stopped_rocks)
     line = read_file(file_name)[0]
     jet = Jet.new(line.split '')
 
     rock_templates = [
-        [[0, 0], [1, 0], [2, 0], [3, 0]]
+        [[0, 0], [1, 0], [2, 0], [3, 0]],
+        [[1, 0], [0, 1], [1, 1], [2, 1], [1, 2]],
+        [[0, 0], [1, 0], [2, 0], [2, 1], [2, 2]],
+        [[0, 0], [0, 1], [0, 2], [0, 3]],
+        [[0, 0], [0, 1], [1, 0], [1, 1]],
     ]
     ceiling = Ceiling.new(rock_templates)
-
     chamber = Chamber.new([])
-    rock = ceiling.next(2, chamber.height + 3)
-    # rock affected by gust
-    rock.push(jet.next, chamber)
-    moved = rock.fall(chamber)
-    binding.pry
+    stopped_rocks = 0
 
+    loop do
+        rock = ceiling.next(2, chamber.height + 4)
+        moved = true
+        while moved do
+            rock.push(jet.next, chamber)
+            moved = rock.fall(chamber)
+            # binding.pry
+            if !moved
+                chamber.tiles += rock.tiles
+                stopped_rocks += 1
+
+                if stopped_rocks >= target_stopped_rocks
+                    # draw(chamber)
+                    # binding.pry
+                    return chamber.height
+                end
+            end
+        end
+    end
 end
-
-test_equals(height_after('input17-example.txt', 2022), 3068)
+test_equals(height_after('input17-example.txt', 1), 1)
+test_equals(height_after('input17-example.txt', 2), 4)
+test_equals(height_after('input17-example.txt', 3), 6)
+test_equals(height_after('input17-example.txt', 4), 7)
+test_equals(height_after('input17-example.txt', 5), 9)
+test_equals(height_after('input17-example.txt', 6), 10)
+test_equals(height_after('input17-example.txt', 7), 13)
+test_equals(height_after('input17-example.txt', 8), 15)
+test_equals(height_after('input17-example.txt', 9), 17)
+test_equals(height_after('input17-example.txt', 10), 17)
+# test_equals(height_after('input17-example.txt', 2022), 3068)
+puts "part 1 #{height_after('input17.txt', 2022)}"
